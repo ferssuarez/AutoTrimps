61accd88eb491cd27d96b824897ad85128aff6d5
slivermasterz <lokmanslivermaster@gmail.com>
2018-06-30 20:23:23 -0700

    Looting

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -302,7 +302,7 @@ function userscripts()
         autoTrimpSettings["BuyWeapons"].enabled = true;
         autoTrimpSettings["AutoMaps"].value = 1;
     }
-    /**
+    
     //AutoAllocate Looting II
     if (!perked && game.global.world !== 230){
         viewPortalUpgrades();
@@ -319,7 +319,7 @@ function userscripts()
             message("Done buying Looting II","Notices");
         }
     }
-    */
+    
 }
 
 
