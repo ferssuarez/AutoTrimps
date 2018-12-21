b85d8ca63504bcaf196d030a6fa8367c3479b96f
slivermasterz <lokmanslivermaster@gmail.com>
2018-12-21 15:25:02 -0800

    Spire Builder added to AT

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -18,11 +18,12 @@ var ATversion = '2.1.7.1'; //when this increases it forces users setting update 
 
 var local = false;
 //local = true;
-var ver = "52.22";
-var verDate = "9.12.18";
+var ver = "52.23";
+var verDate = "21.12.18";
 
 var changelogList = [];
-changelogList.push({date: "9.12.2018", version: "", description: "Max Void Fixed", isNew: true});
+changelogList.push({date: "21.12.2018", version: "", description: "Spire Import Readded", isNew: true});
+changelogList.push({date: "9.12.2018", version: "", description: "Max Void Fixed", isNew: false});
 changelogList.push({date: "18.11.2018", version: "", description: "Jobs reworked." , isNew: false});
 changelogList.push({date: "17.11.2018", version: "", description: "Added Income weight to Auto Allocate. Leave it at 0 unless you're deep running one of the big 3 C2s." , isNew: true});
 changelogList.push({date: "14.11.2018", version: "", description: "Improved Auto Allocate fuel zones range." , isNew: true});
@@ -164,7 +165,15 @@ function startAT() {
             return result;
         };
     })();
-    
+
+    let script = document.createElement('script');
+    script.id = 'SpireBuilder-script';
+    script.src = 'https://slivermasterz.github.io/TrimpScripts/SpireBuilder.js';
+    script.setAttribute('crossorigin',"anonymous");
+    document.head.appendChild(script);
+
+
+
     debug('AutoTrimps loaded!');
 }
 
