68373e56cc3f42023353549faa073f3caebab2b3
slivermasterz <lokmanslivermaster@gmail.com>
2019-03-01 23:18:09 -0800

    update version for nursery

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -18,7 +18,7 @@ var ATversion = '2.1.7.1'; //when this increases it forces users setting update 
 
 var local = false;
 //local = true;
-var ver = "52.42";
+var ver = "52.43";
 var verDate = "17.1.19";
 
 var changelogList = [];
