3b5e88a3175265515955b5d8970ef2e76dbc6a28
slivermasterz <lokmanslivermaster@gmail.com>
2019-03-24 14:03:01 -0700

    Fixed heirloom problem

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -25,9 +25,9 @@ function sortHeirlooms(){
 
 //NEW:
 //makes an array of heirlooms sitting in the temporary extra area to indicate to the autoHeirlooms() function which to Carry/Drop
-var worth2 = {'Shield': [], 'Staff': []};
+var worth2 = {'Shield': [], 'Staff': [], 'Core' : []};
 function worthOfHeirlooms(){
-    worth2 = {'Shield': [], 'Staff': []};
+    worth2 = {'Shield': [], 'Staff': [], 'Core ': []};
     for (var index in game.global.heirloomsExtra) {
         var theLoom = game.global.heirloomsExtra[index];
         var data = {'location': 'heirloomsExtra', 'index': index, 'rarity': theLoom.rarity, 'eff': evaluateHeirloomMods(index, 'heirloomsExtra')};
